7dd339abf60d0fe23b19598a55461d5aab0b29a4
Yuto <1174x804@gmail.com>
2026-03-23 01:13:46 +0700

    initial commit

---
+++ car-rental/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1>INITIAL COMMIT!</h1>
+    <p>Let's go we will make this in time!</p>
+    </>
+  );
+}
